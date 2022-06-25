81d6702606c8c11a0d9e8256b627cbb7c52dc9c5
Michael Presley <mipresley23@gmail.com>
2022-06-25 13:26:43 -0400

    start image seed add assets folder for images

--- backend/db/seeders/20220624230525-imageSeeder.js
+++ backend/db/seeders/20220624230525-imageSeeder.js
@@ -9,7 +9,126 @@ module.exports = {
       Example:
       */
       return queryInterface.bulkInsert('Images', [
-
+          {
+            url: 'https://onboat.co/wp-content/uploads/2021/09/cocokai-1.jpg',
+            spotId: 1
+          },
+          {
+            url: '',
+            spotId: 2
+          },
+          {
+            url: '',
+            spotId: 3
+          },
+          {
+            url: '',
+            spotId: 4
+          },
+          {
+            url: '',
+            spotId: 5
+          },
+          {
+            url: '',
+            spotId: 6
+          },
+          {
+            url: '',
+            spotId: 7
+          },
+          {
+            url: '',
+            spotId: 8
+          },
+          {
+            url: '',
+            spotId: 9
+          },
+          {
+            url: '',
+            spotId: 10
+          },
+          {
+            url: '',
+            spotId: 11
+          },
+          {
+            url: '',
+            spotId: 12
+          },
+          {
+            url: '',
+            spotId: 13
+          },
+          {
+            url: '',
+            spotId: 14
+          },
+          {
+            url: '',
+            spotId: 15
+          },
+          {
+            url: '',
+            spotId: 16
+          },
+          {
+            url: '',
+            spotId: 17
+          },
+          {
+            url: '',
+            spotId: 18
+          },
+          {
+            url: '',
+            spotId: 19
+          },
+          {
+            url: '',
+            spotId: 20
+          },
+          {
+            url: '',
+            spotId: 21
+          },
+          {
+            url: '',
+            spotId: 22
+          },
+          {
+            url: '',
+            spotId: 23
+          },
+          {
+            url: '',
+            spotId: 24
+          },
+          {
+            url: '',
+            spotId: 25
+          },
+          {
+            url: '',
+            spotId: 26
+          },
+          {
+            url: '',
+            spotId: 27
+          },
+          {
+            url: '',
+            spotId: 28
+          },
+          {
+            url: '',
+            spotId: 29
+          },
+          {
+            url: '',
+            spotId: 30
+          },
       ], {});
   },
 
