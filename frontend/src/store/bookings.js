eb96d3453cf7e303248aa9ca7f77e81298461eb9
Michael Presley <mipresley23@gmail.com>
2022-06-28 16:09:11 -0400

    create booking works but not bug free save point incase modal is failure

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -2,11 +2,28 @@ import { csrfFetch } from "./csrf"
 
 //type
 const GET_BOOKINGS = 'bookings/GET_BOOKINGS'
+const CREATE_BOOKINGS = 'bookings/CREATE_BOOKINGS'
+const DELETE_BOOKINGS = 'bookings/DELETE_BOOKINGS'
+
 //action creator
 const actionGetBookings = (bookings) => {
   return {
     type: GET_BOOKINGS,
     bookings
+  }
+}
+
+const actionCreateBooking = (booking) => {
+  return {
+    type: CREATE_BOOKINGS,
+    booking
+  }
+}
+
+const actionDeleteBooking = (booking) => {
+  return {
+    type: DELETE_BOOKINGS,
+    booking
   }
 }
 
@@ -24,6 +41,28 @@ export const thunkGetBookings = (bookings) => async(dispatch) => {
   }
 }
 
+export const thunkCreateBooking = (booking) => async (dispatch) => {
+  const response = await csrfFetch("/api/bookings", {
+    method: "POST",
+    headers: {'Content-Type': "application/json"},
+    body: JSON.stringify(booking)
+  });
+  if (response.ok){
+    const newBooking = await response.json();
+    dispatch(actionCreateBooking(newBooking));
+    return newBooking;
+  }
+};
+
+export const thunkDeleteBooking = (bookingId) => async(dispatch) => {
+  const res = await csrfFetch(`/api/bookings/${bookingId}`, {
+    method: "DELETE"
+  })
+  const data = await res.json();
+  dispatch(actionDeleteBooking(data))
+  return res;
+}
+
 //reducer
 const bookingsReducer = (state = {}, action) => {
   const newState = {...state}
@@ -33,6 +72,14 @@ const bookingsReducer = (state = {}, action) => {
       newState[booking.id] = booking
     })
     return newState;
+
+    case CREATE_BOOKINGS:
+      newState[action.booking.id] = action.booking
+      return newState
+
+    case DELETE_BOOKINGS:
+      delete newState[action.booking.id]
+      return newState;
 
     default:
       return state;
