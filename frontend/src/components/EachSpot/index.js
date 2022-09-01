c2534238f4d18cc92f53f37c6645583dc311ca6b
Michael Presley <mipresley23@gmail.com>
2022-09-01 13:13:17 -0400

    each spot images set booking form placed

--- frontend/src/components/EachSpot/index.js
+++ frontend/src/components/EachSpot/index.js
@@ -5,6 +5,7 @@ import { thunkGetOneSpot, thunkDeleteSpot, thunkGetAllSpots, thunkEditSpot} from
 import { thunkCreateBooking, thunkDeleteBooking, thunkGetBookings } from "../../store/bookings";
 import { thunkGetImages } from "../../store/images";
 import { thunkGetMarinas } from "../../store/marinas";
+import { thunkGetAllUsers } from "../../store/users";
 import '../../index.css';
 import './eachSpot.css';
 
@@ -24,10 +25,11 @@ export default function EachSpot() {
   const [ShowBookingForm, setShowBookingForm] = useState(false);
   const [bookings, setBookings] = useState([])
   const [marinas, setMarinas] = useState([]);
+  const [users, setUsers] = useState([]);
   const [errors, setErrors] = useState([]);
 
 
-
+  const userSelector = useSelector(state => state.users)
   const spotSelector = useSelector(state => state.spots);
   const imageSelector = useSelector(state => state.images);
   const sessionUser =  useSelector(state => state.session.user);
@@ -38,6 +40,7 @@ export default function EachSpot() {
   const image = images.find(image => image.id === +id);
   const marina = marinas.find(marina => marina.id === spot.marinaId)
   console.log('marina: ', marina)
+  console.log('spot', spot)
 
 
   const correctUser = () => {
@@ -50,48 +53,51 @@ export default function EachSpot() {
   const todaysMonth = (todaysFullDate.getMonth() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
   const currentYear = todaysFullDate.getFullYear()
 
+  const thisSpotsUser = users && users.find(user => user.id === spot.userId)
+  console.log('this user: ', thisSpotsUser)
 
   const spotsWithBookings = bookings && bookings.map(booking => {
     return [booking.spotId, booking.startDate, booking.endDate];
   })
-  console.log('spotsWithBookings', spotsWithBookings)
-  console.log('id:', id, 'type', typeof id);
 
-const thisSpotsBookings = []
-spotsWithBookings && spotsWithBookings.forEach(ele => {
-  if(ele[0] === +id) thisSpotsBookings.push(ele)
-})
-console.log('bookingsforthisspot: ', thisSpotsBookings)
 
-const findDateRange = (d1, d2) => {
-  const date = new Date(d1);
-  const dates = [];
-  while(date <= new Date(d2)) {
-    dates.push(new Date(date).toISOString().slice(0, 10));
-    date.setDate(date.getDate() + 1);
+
+  const thisSpotsBookings = []
+  spotsWithBookings && spotsWithBookings.forEach(ele => {
+    if(ele[0] === +id) thisSpotsBookings.push(ele)
+  })
+
+
+  const findDateRange = (d1, d2) => {
+    const date = new Date(d1);
+    const dates = [];
+    while(date <= new Date(d2)) {
+      dates.push(new Date(date).toISOString().slice(0, 10));
+      date.setDate(date.getDate() + 1);
+    }
+    return dates
   }
-  console.log('dates: ', dates)
-  return dates
-}
-console.log('date check:', new Date('2022-07-06') < new Date('2022-07-09'))
 
-const bookRanges = thisSpotsBookings && thisSpotsBookings.map(el => {
-  return findDateRange(el[1], el[2])
-})
-console.log('range: ', bookRanges)
-const realRange = [];
-for(let i = 0; i< bookRanges.length; i++){
-  let el = bookRanges[i]
-  for(let j = 0; j < el.length; j++){
-    let ele = el[j]
-    realRange.push(ele)
+  const bookRanges = thisSpotsBookings && thisSpotsBookings.map(el => {
+    return findDateRange(el[1], el[2])
+  })
+  console.log('range: ', bookRanges)
+  const realRange = [];
+  for(let i = 0; i< bookRanges.length; i++){
+    let el = bookRanges[i]
+    for(let j = 0; j < el.length; j++){
+      let ele = el[j]
+      realRange.push(ele)
+    }
   }
-}
 
-console.log('real date range: ', realRange);
+  useEffect(() => {
+    dispatch(thunkGetAllUsers())
+  },[dispatch])
 
-
-
+  useEffect(() => {
+    setUsers(Object.values(userSelector))
+  },[userSelector])
 
   useEffect(() => {
     dispatch(thunkGetAllSpots())
@@ -203,79 +209,76 @@ if(!marina) return null;
 // if(!images) return null;
 
 return(
-  <div id="each-spot-main-container">
-    <div className="each-spot-card">
-      <h1 id="spot-name-title">{spot.name}</h1>
-      <img id='each-spot-image' src={image && image.url} alt="spot"></img>
-      <table id="spot-info-table">
-        <thead>
-          <tr>
-            <th>Price</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>State</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{`$${spot.price}/night`}</td>
-            <td>{marina.address}</td>
-            <td>{marina.city}</td>
-            <td>{marina.state}</td>
-          </tr>
-        </tbody>
-      </table>
+  <>
+    <div id="each-spot-main-content">
+      <div className="each-spot-header-info">
+        <h2 id="spot-name-title">{spot.name}</h2>
+        <div id="spot-header-labels">
+          <p id="spot-header-hosted-by">Hosted by: {thisSpotsUser.username}</p>
+          <p id="spot-header-location">{marina.city}, {marina.state}, {marina.country}</p>
+        </div>
+      </div>
+      <div id="spot-images-container">
+        <img className='each-spot-images' id='each-spot-image-one' src={image && image.url} alt="spot"></img>
+        <div id="spot-images-column-two">
+          <img className='each-spot-images' id='each-spot-image-two' src={image && image.url} alt="spot"></img>
+          <img className='each-spot-images' id='each-spot-image-three' src={image && image.url} alt="spot"></img>
+        </div>
+        <div id="spot-images-column-three">
+          <img className='each-spot-images' id='each-spot-image-four' src={image && image.url} alt="spot"></img>
+          <img className='each-spot-images' id='each-spot-image-five' src={image && image.url} alt="spot"></img>
+        </div>
+      </div>
       <div id="each-spot-button-container">
-        <button class='each-spot-buttons' id='back-to-spots-button' type="button" onClick={handleReloadSpots}>Back to Spots</button>
-        {sessionUser && <button class='each-spot-buttons' id="book-spot-button" type="button" onClick={handleBookingSpotButton}>Book this Spot</button>}
+        {/* <button class='each-spot-buttons' id='back-to-spots-button' type="button" onClick={handleReloadSpots}>Back to Spots</button> */}
         {sessionUser && correctUser() && <div id="edit-delete-button-container"><button class='each-spot-buttons' type="button" onClick={handleEditSpotButton}>Edit this Spot</button>
         <button class='each-spot-buttons' type="button" onClick={handleDelete}>Delete Spot</button></div>}
       </div>
+      <section className="edit-spot-form-container">
+      {!ShowBookingForm && showEditForm && <form className="edit-spot-form" onSubmit={handleEditSubmit}>
+        <input
+          type="text"
+          placeholder={spot.name}
+          value={spotName}
+          defaultValue={spot.name}
+          required
+          onChange={(e) => setSpotName(e.target.value)} />
+        <input
+          type="number"
+          placeholder={spot.price}
+          defaultValue={spot.price}
+          min="0"
+          value={spotPrice}
+          onChange={(e) => setSpotPrice(e.target.value)} />
+        <button class='each-spot-buttons' id='edit-spot-submit-button' type="submit">Submit Edit</button>
+        <button class='each-spot-buttons' id='edit-spot-cancel-button' type="button" onClick={() => setShowEditForm(false)}>Cancel</button>
+      </form>}
+      <section className="booking-form-container">
+
+            <form className="create-booking-form" onSubmit={handleBookingSubmit}>
+              <ul id="booking-errors-list">
+                {errors && errors.map(err => <li>{`${err}`}</li>)}
+              </ul>
+              <input
+                type="date"
+                required
+                value={bookingStartDate}
+                min={`${currentYear}-${todaysMonth}-${todaysDate}`}
+                onChange={(e) => setBookingStartDate(e.target.value)} />
+
+              <input
+                type="date"
+                required
+                value={bookingEndDate}
+                min={bookingStartDate}
+                onChange={(e) => setBookingEndDate(e.target.value)} />
+              <button class='each-spot-buttons' id='book-spot-submit-button' type="submit">Create</button>
+              <button className="each-spot-buttons" id="book-spot-cancel-button" type="button" onClick={() => setShowBookingForm(false)}>Cancel</button>
+            </form>
+          </section>
+      </section>
     </div>
-    <section className="edit-spot-form-container">
-    {!ShowBookingForm && showEditForm && <form className="edit-spot-form" onSubmit={handleEditSubmit}>
-      <input
-        type="text"
-        placeholder={spot.name}
-        value={spotName}
-        defaultValue={spot.name}
-        required
-        onChange={(e) => setSpotName(e.target.value)} />
-      <input
-        type="number"
-        placeholder={spot.price}
-         defaultValue={spot.price}
-        min="0"
-        value={spotPrice}
-        onChange={(e) => setSpotPrice(e.target.value)} />
-      <button class='each-spot-buttons' id='edit-spot-submit-button' type="submit">Submit Edit</button>
-      <button class='each-spot-buttons' id='edit-spot-cancel-button' type="button" onClick={() => setShowEditForm(false)}>Cancel</button>
-    </form>}
-    <section className="booking-form-container">
-
-          {!showEditForm && ShowBookingForm && <form className="create-booking-form" onSubmit={handleBookingSubmit}>
-            <ul id="booking-errors-list">
-              {errors && errors.map(err => <li>{`${err}`}</li>)}
-            </ul>
-            <input
-              type="date"
-              required
-              value={bookingStartDate}
-              min={`${currentYear}-${todaysMonth}-${todaysDate}`}
-              onChange={(e) => setBookingStartDate(e.target.value)} />
-
-            <input
-              type="date"
-              required
-              value={bookingEndDate}
-              min={bookingStartDate}
-              onChange={(e) => setBookingEndDate(e.target.value)} />
-            <button class='each-spot-buttons' id='book-spot-submit-button' type="submit">Create</button>
-            <button className="each-spot-buttons" id="book-spot-cancel-button" type="button" onClick={() => setShowBookingForm(false)}>Cancel</button>
-          </form>}
-        </section>
-    </section>
-  </div>
+  </>
 )
 
 }
