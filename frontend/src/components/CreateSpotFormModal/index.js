fb80db200448c9110b3bacf8480fdd4ced1cd2c7
Michael Presley <mipresley23@gmail.com>
2022-09-06 10:17:45 -0400

    made create spot form to modal with new required input fields works but still needs error handles

--- frontend/src/components/CreateSpotFormModal/index.js
+++ frontend/src/components/CreateSpotFormModal/index.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Modal } from '../../context/Modal';
+import { ReviewModal } from '../../context/ReviewModal';
 import CreateSpotForm from './createSpotForm';
-import '../spots/spots.css'
 
 function CreateSpotFormModal() {
   const [showModal, setShowModal] = useState(false);
-  const sessionUser = useSelector((state) => state.session.user);
+
+  const sessionUser = useSelector(state => state.session.user)
 
   return (
     <>
-      {/* {sessionUser && <button id='createSpotModal' onClick={() => setShowModal(true)}>Create Spot</button>} */}
-       {sessionUser && showModal && (
-      <Modal  onClose={() => setShowModal(false)}>
-          <CreateSpotForm />
-        </Modal>
+      {sessionUser && <button id='create-spot-modal-button' onClick={() => setShowModal(true)}>Create Spot</button>}
+
+       {showModal && (
+      <ReviewModal  onClose={() => setShowModal(false)}>
+          <CreateSpotForm setShowModal={setShowModal} />
+        </ReviewModal>
       )}
     </>
   );
